304709fc822e4fe32182a37196da3aaed490520f
CRAIGSPC\craig <craig@optimalworks.net>
2018-04-21 15:40:27 +0100

    hide contact form when offline

--- src/js/main/enquiry.js
+++ src/js/main/enquiry.js
@@ -42,7 +42,7 @@
       function(err, url, data) {
 
         // invalid request?
-        if (err || !data || !data._e) return;
+        if (err || !data || typeof data !== 'object') return;
 
         // initialise form
         this.postback = data;
